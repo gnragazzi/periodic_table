887ad3ef5ad120b916cbf741c2e20ba76ea44ebd
Gerardo Ragazzi <gnragazzi@gmail.com>
2023-01-19 08:36:37 -0300

    feat: add check for element.sh arguments

--- element.sh
+++ element.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c";
+if [[ -z $1 ]]
+then
+    #if no arguments echo message
+    echo "Please provide an element as an argument.";
+else
+    #check if argument contains a letter (hence, not being a valid atomic number)
+    
+fi
